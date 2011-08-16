80ac0f084743a83a0707a360af4986347e163a15
unknown <PCUser@.(none)>
2011-08-16 11:32:12 +0100

    show player and stair on map

--- script/Mapper.js
+++ script/Mapper.js
@@ -35,15 +35,34 @@ Mapper.prototype = {
 		ctx.fillStyle = "Black";
 		ctx.fillRect(0, 0, this.canvasWidth, this.canvasHeight);
 
-		ctx.fillStyle = "White";
 
 		for (var y=0; y < ySize; y++) {
 		for (var x=0; x < xSize; x++) {
+				if (map[z][y][x] == 1)
+					continue;
+					
 				if (map[z][y][x] == 0) {
+					ctx.fillStyle = "White";
+					ctx.fillRect(x*xWidth, (ySize-1-y)*yWidth, xWidth, yWidth);
+				}
+				if (2 <= map[z][y][x] && map[z][y][x] <= 5) {
+					ctx.fillStyle = "Yellow";
+					ctx.fillRect(x*xWidth, (ySize-1-y)*yWidth, xWidth, yWidth);
+				}
+				if (6 <= map[z][y][x] && map[z][y][x] <= 9) {
+					ctx.fillStyle = "Blue";
 					ctx.fillRect(x*xWidth, (ySize-1-y)*yWidth, xWidth, yWidth);
 				}
 			};
 		};
+		
+		var xCenter = (game.xPos) * xWidth;
+		var yCenter = ((ySize-game.yPos)) * yWidth;
+
+		ctx.fillStyle = "Red";
+		ctx.arc(xCenter, yCenter, xWidth/2, game.direction+Math.PI*5/4, game.direction+Math.PI*7/4, false);
+		ctx.fill();
+		
 		
 	}
 }
