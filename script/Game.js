0121e87723994bb64f8d7f0cfa75ca6c5da08616
unknown <PCUser@.(none)>
2011-08-29 17:19:31 +0200

    tidy

--- script/Game.js
+++ script/Game.js
@@ -10,7 +10,16 @@ Game.YSIZE = 10;
 Game.ZSIZE = 4;
 
 Game.prototype = {
-		initialize : function() {
+	initialize : function() {
+		this.setupHTML();
+		this.setupWebGL();
+		this.setupMaze();
+		this.setupActor();
+		this.setupMapper();
+				
+		this.draw();	
+	},
+	setupHTML : function() {
 		document.getElementById("main").innerHTML = 
 				'<canvas id="canvas" width="960" height="540"></canvas>' +
 				'<div id="menu" class="window">' +
@@ -19,7 +28,8 @@ Game.prototype = {
 				'	<p onclick="gm.newScene(0)">EXIT</p>' +
 				'</div>' +
 				'<canvas id="map" class="window" width="507" height="507" style="display:none" onclick=hidemap()></div>';
-	
+	},
+	setupWebGL : function() {
 		var canvas = document.getElementById("canvas");
 		initGL(canvas);
 		initShaders()
@@ -28,98 +38,6 @@ Game.prototype = {
 		gl.clearColor(0.0, 0.0, 1.0, 1.0);
 		gl.enable(gl.DEPTH_TEST);
 		
-		actorModel = new ActorModel();
-		
-
-		this.key = 0;
-
-		this.xPos = 1.5;
-		this.yPos = 1.5;
-		this.zPos = 0.0;
-		this.direction = 0;
-
-		this.floor = 0;
-		
-		this.mapDisplay = "none";
-		
-		this.map = new MazeMap();
-
-		this.actors = new Array();
-		var i=0;
-		for (var z=0; z<Game.ZSIZE-1; z++) {
-		for (var y=1; y<Game.YSIZE-1; y++) {
-		for (var x=1; x<Game.XSIZE-1; x++) {
-			if (this.map.map[z][y][x] != 0)
-				continue;
-				
-			this.actors[i] = new Actor(x, y, z);
-			i++;
-		}
-		}
-		}
-		this.actorNum = i;
-
-		this.initModel();
-			
-		this.map.walked[Math.floor(this.zPos)][Math.floor(this.yPos)][Math.floor(this.xPos)] = 1;
-
-		this.updateFloorStatus(this.floor);
-		
-		this.actor = new Actor();
-		
-		var mapCanvas = document.getElementById("map");
-		this.mapper = new Mapper(mapCanvas);
-		
-		this.draw();	
-	},
-	update : function() {
-
-		this.inputProc();
-		
-		for (var i=0; i < this.actorNum; i++) {
-			this.actors[i].update(this.map);
-		}
-
-	},
-	inputProc : function() {
-		switch(gm.key) {
-			case 0:
-				return;
-				
-			// a:left
-			case 65:
-			case 37:
-				this.turn(-1);
-				break;
-
-			// w:up
-			case 87:
-			case 38:
-				this.movePlayer(1);
-				break;
-
-			// d:right
-			case 68:
-			case 39:
-				this.turn(1);
-				break;
-
-			// s:down
-			case 83:
-			case 40:
-				this.movePlayer(-1);
-				break;
-				
-			// space
-			case 32:
-				this.toggleMapDisplay();
-				break;
-		}
-
-		gm.key = 0;
-	},
-	initModel : function() {
-
 		mat4.perspective(45, gl.viewportWidth / gl.viewportHeight, 0.1, 100.0, pMatrix);
 		cameraPos = [0.0, 0.0, -0.3];
 		cameraRotX = -90;
@@ -146,33 +64,87 @@ Game.prototype = {
 		gl.uniform3f(shaderProgram.pointLightingLocationUniform, vCamera[0], vCamera[1], vCamera[2]);
 
 		gl.uniform3f(shaderProgram.pointLightingColorUniform, 1.0, 1.0, 1.0);
-
 	},
-	draw : function() {
-		gl.viewport(0, 0, gl.viewportWidth, gl.viewportHeight);
-		gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+	setupMaze : function() {
+		this.xPos = 1.5;
+		this.yPos = 1.5;
+		this.zPos = 0.0;
+		this.direction = 0;
 
-		gl.activeTexture(gl.TEXTURE0);
-		gl.bindTexture(gl.TEXTURE_2D, neheTexture);
+		this.floor = 0;
+		
+		this.map = new MazeMap();
 
-		mat4.identity(mvMatrix);
+		this.map.walked[Math.floor(this.zPos)][Math.floor(this.yPos)][Math.floor(this.xPos)] = 1;
 
-		mat4.rotate(mvMatrix, degToRad(cameraRotX), [1, 0, 0]);
-		mat4.rotate(mvMatrix, degToRad(cameraRotY), [0, 1, 0]);
-		mat4.rotate(mvMatrix, degToRad(cameraRotZ), [0, 0, 1]);
-		mat4.translate(mvMatrix, cameraPos);
+		this.updateFloorStatus(this.floor);
+	},
+	setupActor : function() {
+		actorModel = new ActorModel();
 
-		mat4.rotate(mvMatrix, this.direction, [0, 0, 1]);
-		mat4.translate(mvMatrix, [-this.xPos*Game.SCALE, -this.yPos*Game.SCALE, -this.zPos*Game.SCALE]);
+		this.actor = new Actor();
 
-		this.map.draw();
+		this.actors = new Array();
+		var i=0;
+		for (var z=0; z<Game.ZSIZE-1; z++) {
+		for (var y=1; y<Game.YSIZE-1; y++) {
+		for (var x=1; x<Game.XSIZE-1; x++) {
+			if (this.map.map[z][y][x] != 0)
+				continue;
+				
+			this.actors[i] = new Actor(x, y, z);
+			i++;
+		}
+		}
+		}
+		this.actorNum = i;
+	},
+	setupMapper : function() {
+		var mapCanvas = document.getElementById("map");
+		this.mapper = new Mapper(mapCanvas);
+		this.mapDisplay = "none";
+	},
+	update : function() {
 
+		this.inputProc();
+		
 		for (var i=0; i < this.actorNum; i++) {
-			this.actors[i].draw();
+			this.actors[i].update(this.map);
 		}
 
-		this.mapper.draw(this.map.map, this.map.walked, this.xPos, this.yPos, this.floor, this.direction);
+	},
+	inputProc : function() {
+		switch(gm.key) {
+			case 0:
+				return;
+				
+			// a:left
+			case 65:
+			case 37:
+				this.turn(-1);
+				break;
+			// w:up
+			case 87:
+			case 38:
+				this.movePlayer(1);
+				break;
+			// d:right
+			case 68:
+			case 39:
+				this.turn(1);
+				break;
+			// s:down
+			case 83:
+			case 40:
+				this.movePlayer(-1);
+				break;
+			// space
+			case 32:
+				this.toggleMapDisplay();
+				break;
+		}
 
+		gm.key = 0;
 	},
 	movePlayer : function(d) {
 		var dx = d * Math.sin(this.direction) * 0.05;
@@ -221,5 +193,31 @@ Game.prototype = {
 			this.mapDisplay = "none";
 			
 		document.getElementById("map").style.display = this.mapDisplay;
-	}
+	},
+	draw : function() {
+		gl.viewport(0, 0, gl.viewportWidth, gl.viewportHeight);
+		gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+		gl.activeTexture(gl.TEXTURE0);
+		gl.bindTexture(gl.TEXTURE_2D, neheTexture);
+
+		mat4.identity(mvMatrix);
+
+		mat4.rotate(mvMatrix, degToRad(cameraRotX), [1, 0, 0]);
+		mat4.rotate(mvMatrix, degToRad(cameraRotY), [0, 1, 0]);
+		mat4.rotate(mvMatrix, degToRad(cameraRotZ), [0, 0, 1]);
+		mat4.translate(mvMatrix, cameraPos);
+
+		mat4.rotate(mvMatrix, this.direction, [0, 0, 1]);
+		mat4.translate(mvMatrix, [-this.xPos*Game.SCALE, -this.yPos*Game.SCALE, -this.zPos*Game.SCALE]);
+
+		this.map.draw();
+
+		for (var i=0; i < this.actorNum; i++) {
+			this.actors[i].draw();
+		}
+
+		this.mapper.draw(this.map.map, this.map.walked, this.xPos, this.yPos, this.floor, this.direction);
+
+	},
 }
